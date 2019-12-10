7329766f344ccceba5c16bfba52db1b2d6395e5c
damian damian <dbrpk@o2.pl>
2019-12-10 12:37:27 +0100

    'kontynuuaacja'

--- tstring.h
+++ tstring.h
@@ -1,59 +1,47 @@
-#include "tstring.h"
 #ifndef TSTRING_H
+
 #define TSTRING_H
+
 #include <cstring>
-class TString {
- public:
- char*begin(){return ptr;}
- char* end(){return ptr+len;}
- size_t lenght()const {return len;}
- void clear(){delete[]ptr;ptr=nullptr;len=0;}
- bool empty()const{return len ? false: true;}
- char&front(){return*ptr;}
- const char&front()const{return*ptr;}
- char&back(){return*(ptr+len-1);}
- const char&backconst() {return*(ptr+len-1);}
 
- size_t size()const{return len;}
- char* insert(size_t pos, const char* c){
-if (pos >=0 && pos <= len) {
-size_t oldlen = len;
-len = len+strlen(c);
-char* tmp = new char[ len+1 ];
-strcpy( tmp, ptr );
-for (size_t i=pos; i<pos+strlen(c); ++i) {
-tmp[i] = c[i-pos];
-}
-for (size_t i=pos; i<oldlen; ++i) {
-tmp[i+strlen(c)] = ptr[i];
-}
-delete [] ptr;
-ptr = tmp;
-return ptr+pos;
-} else {
-throw out_of_range("zly argument");
-}
-return ptr;
- 
- }
- char* insert(size_t pos, char c);
- char* erase(size_t bpos=0, size_t len=0);
+#include "operatory.h"
+//by RK
+class TString{
+    public:
 
- TString(const char* s = nullptr);
-TString(const TString& s);
-TString& operator= (const TString& s);
-~TString();
-TString& operator= (TString &&s);
+	TString(const char* s = nullptr);
+	TString(const TString& s);
+	TString& operator= (const TString& s);
+	TString& operator= (TString&& s);
+   
+    char& operator[](size_t n);
+	const char& operator[](size_t n)const;
+	  
+	char* begin() {return ptr;}
+	char* end() {return ptr+len;}
+	size_t length() const {return len;}
+	void clear() {delete [] ptr; ptr = nullptr; len = 0;}
+	bool empty() const {return len ? false : true;}
+	char& front() {return *ptr;}
+	const char& front() const {return *ptr;}
+	char& back() {return*(ptr+len-1); }
+	const char& back() const {return *(ptr+len-1);}
+    
+	size_t size () const{return len; }
+	
+	
+    char* insert(size_t pos, const char* c);
+	char* insert(size_t pos, char* c);
+	char* erase(size_t bpos = 0, size_t leng = 0);
+	friend std::ostream& MojeOperatory::operator<< (std::ostream& strumien, const TString& s);
+    friend std::istream& MojeOperatory::operator>> (std::istream& strumien, TString& s );
 
-char& operator[]( size_t n );
-const char& operator[]( size_t n ) const;
-friend std::ostream& MojeOperatory::operator<<( std::ostream& strumien, const TString& s );
- //interfejs
- private:
-    char* ptr = nullptr;
-    std::size_t len = 0;
-    //implementacja
-    //składowe
- protected:
+  
+    ~TString();
+    private:
+    char* ptr=nullptr;
+    std::size_t len =0;
+
+    
 };
-#endif 
+#endif
